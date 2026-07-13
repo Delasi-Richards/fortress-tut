038b5324ee963420f956acd734f983bc57a2d49f
Delasi-Richards <delasi.richards@gmail.com>
2026-07-13 12:19:29 +0000

    fix: Added install/ and log/ folders to .gitignore

--- install/fortress_tut_msgs/share/fortress_tut_msgs/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(fortress_tut_msgs_IDL_FILES "srv/MoveTurtlebot.idl")
-set(fortress_tut_msgs_INTERFACE_FILES "srv/MoveTurtlebot.srv;srv/MoveTurtlebot_Request.msg;srv/MoveTurtlebot_Response.msg")
